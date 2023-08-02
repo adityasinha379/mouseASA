0bc3addeb66a37678646bab97b3e046b69f524f9
Aditya Sinha <sinhaa2@lilac-ln02.hpc.private>
2023-08-01 22:15:34 -0400

    cleaning job submit script

--- scripts/bsub_MouseASA.sh
+++ scripts/bsub_MouseASA.sh
@@ -12,22 +12,16 @@ celltype='cd8'
 poolsize=2
 dropout='0.2'
 weight=1.0
+use_prior=1
 
-# Run Model 3.0
-# for num in {0..4}
-# do
-for use_prior in 1 # {0,1}
+
+for dataset in 'both'
 do
-    for dataset in 'both'
+    for BATCH_SIZE in 16
     do
-        for BATCH_SIZE in 16
-        do
-            logfile="logs/test.out"
-            # logfile="logs/log_${celltype}.${dataset}.BATCHSIZE${BATCH_SIZE}.out"
-            # rm -rf ${logfile}
-            bsub -J ${celltype}.${dataset}.${use_prior}.${BATCH_SIZE} -n 20 -R "A100" -W 5:00 -sla llSC2 -q gpuqueue -gpu "num=1" -o ${logfile} -eo ${logfile} "/data/leslie/sinhaa2/mouseASA/scripts/run_m3.sh $dataset $BATCH_SIZE $celltype $poolsize $dropout $use_prior $weight"
-        done
-        echo "${celltype}.${dataset}.${use_prior}.${BATCH_SIZE}"
+        logfile="logs/log_${celltype}.${dataset}.BATCHSIZE${BATCH_SIZE}.out"
+        rm -rf ${logfile}
+        bsub -J ${celltype}.${dataset}.${weight}.${BATCH_SIZE} -n 10 -R "A100" -sla llSC2 -W 3:00 -q gpuqueue -gpu "num=1" -o ${logfile} -eo ${logfile} "/data/leslie/sinhaa2/mouseASA/scripts/run_m3.sh $dataset $BATCH_SIZE $celltype $poolsize $dropout $use_prior $weight"
     done
+    echo "${celltype}.${dataset}.${weight}.${BATCH_SIZE}"
 done
-# done
