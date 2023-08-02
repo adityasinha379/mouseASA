7a06f7fb638a1c2fdfeef87f6f6d525a05acbce0
Aditya Sinha <sinhaa2@lilac-ln02.hpc.private>
2023-08-01 21:34:13 -0400

    documenting utils, adding README

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,8 +7,15 @@ from bisect import bisect
 from scipy.stats import zscore
 
 def trim_weights_dict(weights):
+    '''
+    LEGACY: Some code I wrote to load weights from a trained pairScan (with FC head) model to a template without the FC head
+    Basically, I was having problems doing feature attribution since pairScan gives two outputs and the FA package hates that
+    But ultimately, I settled on using a wrapper for my model instead during FA.
+    Input: Torch weight dictionary
+    Output: Torch weight dictionary
+    '''
     temp = []
-    for k in weights.keys():
+    for k in weights.keys():       # remove the fchead weights from the loaded weight dictionary
         if k[:6]=='fchead':
             temp.append(k)
     for k in temp:
@@ -16,8 +23,17 @@ def trim_weights_dict(weights):
     return weights
 
 def get_neg_summits(chromsummits, num, chrom_length):
+    '''
+    Code to get "true" uneg summits for a  particular chromosome.
+    This samples unegs uniformly from the background instead of from flanks of peaks and generally performs better when compared to taking flanks
+    Two caveats about this: 1. We make sure samples are at least 10kb from any positive summit, 2. We don't sample from first and last 5Mb of the chromosome (low mappability areas)
+    Input: chromsummits (n,) - list of positive summits
+           num (int) - number of samples needed
+           chrom_length (int) - length of chromosome
+    Output: neg_summits (num,)
+    '''
     neg_summits = np.empty(0, dtype=np.int64)
-    while True:
+    while True:                # Trial and error, sample summits, take only 10kb separated ones, repeat till you have num samples
         temp = np.random.choice(np.arange(5000000, chrom_length-5000000), num, replace=False)
         idx = np.where(np.array([np.min(np.abs(x- chromsummits)) for x in temp])>10000)[0]     # at least 10kb from summit
         neg_summits = np.concatenate((neg_summits, temp[idx]))
@@ -27,6 +43,12 @@ def get_neg_summits(chromsummits, num, chrom_length):
     return neg_summits
 
 def get_confweights(dataset):
+    '''
+    DORMANT: We were trying to weight the fold change head loss (fc_loss) with confidence weights, where a peak is higher confidence if it has higher allelic imbalance
+    To do this Vianne performed a beta-binomial test to give allelic imbalance pvals to each peak (intuitively, low pval if peak has high FC and high coverage)
+    Input: dataset (str) - train, val or test
+    Output: zscores (len,) - list of zscores quantifying allelic imbalance for each sample in the dataset
+    '''
     if dataset=='train':
         sig_path = '/data/leslie/shared/ASA/mouseASA/data/cd8/betabinom_result_combCounts_150bp_trainOnly.csv'
     elif dataset=='val':
@@ -37,14 +59,27 @@ def get_confweights(dataset):
     return zscores
 
 def subsample_unegs(lens, frac=0.5):
-    # for pairScan
-    # given list of uneg dataset lengths, returns subsample indices for each
+    '''
+    For when we want to use a variable fraction of unegs for training
+    Given list of uneg dataset lengths, returns subsample indices for each
+    Input: lens (list of int), frac (float)
+    Output: idx (list of list of int)
+    '''
     idx = []
+    rng = np.random.default_rng(seed=0)
     for i in range(len(lens)): # train, val
-        idx.append(np.sort(np.random.choice(np.arange(lens[i]), size=int(frac*lens[i]), replace=False)))
+        idx.append(np.sort(rng.choice(np.arange(lens[i]), size=int(frac*lens[i]), replace=False)))
     return idx
 
 def get_summits(peaks):
+    '''
+    LEGACY: Old code I wrote that takes peaks dataframe and gets (positive and) negative summits from the peak flanks (500 bp away from peak edge)
+    These negative summits can then be subsampled as you like.
+    Could use some overhauling. Eg. probably dont need to return all columns just chr and summit
+    As mentioned earlier, random sampling from background seems empirically better, but you can use this to compare
+    Input: peaks (pd df) (n,10) loaded from BED file
+    Output: summits (n,10) summits_neg (2n,10)
+    '''
     # Returns summits corresponding to peaks and also for negative flanking regions
     summits = peaks.copy()
     summits.iloc[:,1] += summits.iloc[:,9]
@@ -54,13 +89,18 @@ def get_summits(peaks):
     summits_neg1 = summits.copy()
     for i in range(len(peaks)):
         summits_neg.iloc[i,1] = peaks.iloc[i,2] + 500
-        summits_neg1.iloc[i,1] = peaks.iloc[i,1] - (500-1)
+        summits_neg1.iloc[i,1] = peaks.iloc[i,1] - 500
     
     summits_neg = pd.concat( (summits_neg,summits_neg1), ignore_index=True)
     summits_neg = summits_neg.sort_values(by=[0,1], ignore_index=True)
     return summits, summits_neg
 
 def get_shifts(chromsummits, mods, c):
+    '''
+    For a chromosome, get the shift values to convert each summit to cast coordinate
+    Input: chromsummits (n,), mods (list n), c (int) chromsome
+    Output: cast_shifts (n,)
+    '''
     mod_c = [x for x in mods if '\t'+str(c)+'\t' in x]           # slice out relevant mod lines and arrange them in dataframe
     mod_c = pd.DataFrame([x.split('\t') for x in mod_c])
     mod_c[2] = mod_c[2].astype(int)
@@ -73,58 +113,23 @@ def get_shifts(chromsummits, mods, c):
     return cast_shifts
 
 def one_hot(x):
-    # input: list length n of strings
-    # output: (n,seqlen,4)
+    '''
+    Convert input DNA seqs to one-hot
+    Input: list n of str
+    Output: (n, seqlen, 4)
+    '''
     x_oht = []
-    mapping = dict(zip(['A','C','G','T','N'],[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]]))
+    mapping = dict(zip(['A','C','G','T','N'],[[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1],[0,0,0,0]]))   # Order is IMPORTANT
     for i in range(len(x)):
         x_oht.append([mapping[nuc] for nuc in x[i]])
     return np.array(x_oht)
 
-def GCregress(x, y, method='linear'):
-    # x: (n, seqlen, 4)  y: (n,)
-    GC = np.sum(x, axis=1)
-    GC = GC[:,1]+GC[:,2]           # get total GC content for each OHT coded peakseq
-    if method=='linear':
-        reg = LinearRegression().fit(GC.reshape(-1,1),y.reshape(-1,1))
-        return reg.coef_[0][0]
-    elif method=='spline':
-        splinereg = get_natural_cubic_spline_model(GC, y, minval=np.min(GC), maxval=np.max(GC), n_knots=6)
-        return splinereg.predict(GC)/GC
-
-def revcomp_old(x,y):
-    # for alleleScan
-    # augment data by adding reverse complements
-    # output x: (2n, seqlen, 4)    y: (2n,)
-    x = np.vstack( (x, np.flip(np.flip(x,axis=1),axis=2)) )    # reverse complement the sequences and stack them below already existing peak sequences
-    y = np.concatenate((y,y))                    # simply duplicate the log-summed accessibility RPMs
-    return x,y
-
-def revcomp(x,y):
-    # for pairScan
-    # augment data by adding reverse complements
-    # input x: (n, 2, seqlen, 4)    y: (n, 2)
-    # output x: (2n, 2, seqlen, 4)    y: (2n, 2)
-    x = np.vstack( (x, np.flip(np.flip(x,axis=2),axis=3)) )    # reverse complement the sequences and stack them below already existing peak sequences
-    y = np.vstack((y,y))                    # simply duplicate the log-summed accessibility RPMs
-    return x,y
-
-def remove_blacklist(peaks, blacklist):
-    idx = []
-    for c in np.unique(peaks[0]):
-        chromblack = blacklist.loc[np.where(blacklist[0]=='chr'+str(c))].iloc[:,1:3]
-        chromblack = np.array([x for y in chromblack.values.tolist() for x in y])
-        chrompeaks = peaks.loc[np.where(peaks[0]==c)]
-        temp = chrompeaks.index.tolist()
-        for i in range(len(chrompeaks)):
-            if np.argmax(chrompeaks.iloc[i,1]<chromblack)%2 or np.argmax(chrompeaks.iloc[i,2]<chromblack)%2:
-                idx.append(temp[i])
-    peaks = peaks.drop(idx).reset_index(drop=True)
-    return peaks
-
 def unhot(x):
-    # input shape (n,len,4)
-    # output shape (n,len)
+    '''
+    Convert one-hot input back to str DNA seqs
+    Input: x (n, seqlen, 4)
+    Output: x_dna (n, seqlen)
+    '''
     x_dna = np.full((x.shape[0],x.shape[1]), 'N')
     nucs = ['A','C','G','T']
     for i in range(len(x)):
@@ -134,138 +139,61 @@ def unhot(x):
     x_dna = np.array([''.join(x_dna[i]) for i in range(len(x_dna))])
     return x_dna
 
-def get_natural_cubic_spline_model(x, y, minval=None, maxval=None, n_knots=None, knots=None):
-    """
-    Get a natural cubic spline model for the data.
+def GCregress(x, y):
+    '''
+    Code to perform linear GC regression
+    Input: x (n, seqlen, 4),  y: (n,)
+    Output: reg_coef (float)
+    '''
+    GC = np.sum(x, axis=1)
+    GC = GC[:,1]+GC[:,2]           # get total GC content for each OHT coded peakseq
+    reg = LinearRegression().fit(GC.reshape(-1,1),y.reshape(-1,1))
+    return reg.coef_[0][0]
 
-    For the knots, give (a) `knots` (as an array) or (b) minval, maxval and n_knots.
+def revcomp_m3(x,y):
+    '''
+    Augment input dataset with reverse compliments (for alleleScan)
+    Input: x (n, seqlen, 4), y (n,)
+    Ouput: x (2n, seqlen, 4), y (2n,)
+    '''
+    x = np.vstack( (x, np.flip(np.flip(x,axis=1),axis=2)) )    # reverse complement the sequences and stack them below already existing peak sequences
+    y = np.concatenate((y,y))                    # simply duplicate the log-summed accessibility RPMs
+    return x,y
 
-    If the knots are not directly specified, the resulting knots are equally
-    space within the *interior* of (max, min).  That is, the endpoints are
-    *not* included as knots.
+def revcomp(x,y):
+    '''
+    Augment input dataset with reverse compliments (for pairScan) (TODO: can probably be merged into one function)
+    Input: x (n, 2, seqlen, 4), y (n, 2)
+    Ouput: x (2n, 2, seqlen, 4), y (2n, 2)
+    '''
+    x = np.vstack( (x, np.flip(np.flip(x,axis=2),axis=3)) )    # reverse complement the sequences and stack them below already existing peak sequences
+    y = np.vstack((y,y))                    # simply duplicate the log-summed accessibility RPMs
+    return x,y
 
-    Parameters
-    ----------
-    x: np.array of float
-        The input data
-    y: np.array of float
-        The outpur data
-    minval: float 
-        Minimum of interval containing the knots.
-    maxval: float 
-        Maximum of the interval containing the knots.
-    n_knots: positive integer 
-        The number of knots to create.
-    knots: array or list of floats 
-        The knots.
+def remove_blacklist(peaks, blacklist):
+    '''
+    LEGACY: Code to filter out the blacklisted peaks
+    Input: peaks (n, 10), blacklist (m, 4)
+    Output: peaks (n-m, 10)
+    '''
+    idx = []
+    for c in np.unique(peaks[0]):
+        chromblack = blacklist.loc[np.where(blacklist[0]=='chr'+str(c))].iloc[:,1:3]
+        chromblack = np.array([x for y in chromblack.values.tolist() for x in y])
+        chrompeaks = peaks.loc[np.where(peaks[0]==c)]
+        temp = chrompeaks.index.tolist()
+        for i in range(len(chrompeaks)):
+            if np.argmax(chrompeaks.iloc[i,1]<chromblack)%2 or np.argmax(chrompeaks.iloc[i,2]<chromblack)%2:
+                idx.append(temp[i])
+    peaks = peaks.drop(idx).reset_index(drop=True)     # drop blacklisted peaks
+    return peaks
 
-    Returns
-    --------
-    model: a model object
-        The returned model will have following method:
-        - predict(x):
-            x is a numpy array. This will return the predicted y-values.
-    """
-
-    if knots:
-        spline = NaturalCubicSpline(knots=knots)
-    else:
-        spline = NaturalCubicSpline(max=maxval, min=minval, n_knots=n_knots)
-
-    p = Pipeline([
-        ('nat_cubic', spline),
-        ('regression', LinearRegression(fit_intercept=True))
-    ])
-
-    p.fit(x, y)
-
-    return p
-
-class AbstractSpline(BaseEstimator, TransformerMixin):
-    """Base class for all spline basis expansions."""
-
-    def __init__(self, max=None, min=None, n_knots=None, n_params=None, knots=None):
-        if knots is None:
-            if not n_knots:
-                n_knots = self._compute_n_knots(n_params)
-            knots = np.linspace(min, max, num=(n_knots + 2))[1:-1]
-            max, min = np.max(knots), np.min(knots)
-        self.knots = np.asarray(knots)
-
-    @property
-    def n_knots(self):
-        return len(self.knots)
-
-    def fit(self, *args, **kwargs):
-        return self
-
-class NaturalCubicSpline(AbstractSpline):
-    """Apply a natural cubic basis expansion to an array.
-    The features created with this basis expansion can be used to fit a
-    piecewise cubic function under the constraint that the fitted curve is
-    linear *outside* the range of the knots..  The fitted curve is continuously
-    differentiable to the second order at all of the knots.
-    This transformer can be created in two ways:
-      - By specifying the maximum, minimum, and number of knots.
-      - By specifying the cutpoints directly.  
-
-    If the knots are not directly specified, the resulting knots are equally
-    space within the *interior* of (max, min).  That is, the endpoints are
-    *not* included as knots.
-    Parameters
-    ----------
-    min: float 
-        Minimum of interval containing the knots.
-    max: float 
-        Maximum of the interval containing the knots.
-    n_knots: positive integer 
-        The number of knots to create.
-    knots: array or list of floats 
-        The knots.
-    """
-
-    def _compute_n_knots(self, n_params):
-        return n_params
-
-    @property
-    def n_params(self):
-        return self.n_knots - 1
-
-    def transform(self, X, **transform_params):
-        X_spl = self._transform_array(X)
-        if isinstance(X, pd.Series):
-            col_names = self._make_names(X)
-            X_spl = pd.DataFrame(X_spl, columns=col_names, index=X.index)
-        return X_spl
-
-    def _make_names(self, X):
-        first_name = "{}_spline_linear".format(X.name)
-        rest_names = ["{}_spline_{}".format(X.name, idx)
-                      for idx in range(self.n_knots - 2)]
-        return [first_name] + rest_names
-
-    def _transform_array(self, X, **transform_params):
-        X = X.squeeze()
-        try:
-            X_spl = np.zeros((X.shape[0], self.n_knots - 1))
-        except IndexError: # For arrays with only one element
-            X_spl = np.zeros((1, self.n_knots - 1))
-        X_spl[:, 0] = X.squeeze()
-
-        def d(knot_idx, x):
-            def ppart(t): return np.maximum(0, t)
-
-            def cube(t): return t*t*t
-            numerator = (cube(ppart(x - self.knots[knot_idx]))
-                         - cube(ppart(x - self.knots[self.n_knots - 1])))
-            denominator = self.knots[self.n_knots - 1] - self.knots[knot_idx]
-            return numerator / denominator
-
-        for i in range(0, self.n_knots - 2):
-            X_spl[:, i+1] = (d(i, X) - d(self.n_knots - 2, X)).squeeze()
-        return X_spl
 
 def load_oldcounts():
+    '''
+    LEGACY: Code to load some old counts generated by Vianne for comparison
+    Will never be used by you or me for that matter. Ignore this.
+    '''
     datadir = '/data/leslie/shared/ASA/mouseASA/data/cd8/'
     df = pd.read_csv(datadir+'data_vi_old.csv')
     x_b6 = one_hot(df['b6_seq'])
@@ -281,7 +209,7 @@ def load_oldcounts():
     yTr = np.concatenate((y_b6[tr_idx], y_ca[tr_idx]))
     yVa = np.concatenate((y_b6[va_idx], y_ca[va_idx]))
     yTe = np.concatenate((y_b6[te_idx], y_ca[te_idx]))
-    xTr, yTr = revcomp_old(xTr, yTr)
-    xVa, yVa = revcomp_old(xVa, yVa)
-    xTe, yTe = revcomp_old(xTe, yTe)
+    xTr, yTr = revcomp_m3(xTr, yTr)
+    xVa, yVa = revcomp_m3(xVa, yVa)
+    xTe, yTe = revcomp_m3(xTe, yTe)
     return xTr, xVa, xTe, yTr, yVa, yTe
